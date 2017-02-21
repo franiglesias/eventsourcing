f3c25d93585d6143e37b577ed40d00a23497681d
franiglesias <franiglesias@mac.com>
2017-02-21 13:34:25 +0100

    Preparing for config auto detection

--- spec/Milhojas/EventSourcing/Utility/ConfigManagerSpec.php
+++ spec/Milhojas/EventSourcing/Utility/ConfigManagerSpec.php
@@ -18,6 +18,12 @@ class ConfigManagerSpec extends ObjectBehavior
         $this->shouldHaveType(ConfigManager::class);
     }
 
+    public function it_can_locate_default_configuration_file()
+    {
+        $this->beConstructedWith();
+        $this->getAvailableConnections()->shouldBeArray();
+    }
+
     public function it_can_return_a_list_of_availble_connections()
     {
         $this->getAvailableConnections()->shouldBe(['test', 'production']);
@@ -26,6 +32,12 @@ class ConfigManagerSpec extends ObjectBehavior
     public function it_throws_exception_if_invalid_connection()
     {
         $this->shouldThrow(\InvalidArgumentException::class)->during('getConfiguration', ['invalid']);
+    }
+
+    public function it_throws_exception_if_invalid_configuration_file()
+    {
+        $this->beConstructedWith($this->getInvalidFile());
+        $this->shouldThrow(\InvalidArgumentException::class)->during('getConfiguration', ['test']);
     }
 
     public function it_can_return_options_for_a_given_connection()
@@ -69,7 +81,41 @@ class ConfigManagerSpec extends ObjectBehavior
             ],
         ];
         $fileSystem = vfsStream::setUp('root', 0, []);
-        $file = vfsStream::newFile('database.yml')
+        $file = vfsStream::newFile('config/database.yml')
+            ->withContent(Yaml::dump($config))
+            ->at($fileSystem);
+
+        return $file->url();
+    }
+
+    public function getInvalidFile()
+    {
+        $config = [
+            'dbal' => [
+                'default_connection' => 'test',
+                'connections' => [
+                    'test' => [
+                        'driver' => 'pdo_mysql',
+                        'user' => 'root',
+                        'password' => 'root',
+                        'dbname' => 'testmilhojas',
+                        'host' => 'localhost',
+                        'charset' => 'utf8mb4',
+                    ],
+                    'production' => [
+                        'driver' => 'pdo_mysql',
+                        'user' => 'user',
+                        'password' => 'pwd',
+                        'dbname' => 'milhojas',
+                        'host' => 'localhost',
+                        'charset' => 'utf8mb4',
+                    ],
+
+                ],
+            ],
+        ];
+        $fileSystem = vfsStream::setUp('root', 0, []);
+        $file = vfsStream::newFile('config/invalid.yml')
             ->withContent(Yaml::dump($config))
             ->at($fileSystem);
 
--- src/Milhojas/EventSourcing/Utility/ConfigManager.php
+++ src/Milhojas/EventSourcing/Utility/ConfigManager.php
@@ -8,9 +8,16 @@ class ConfigManager
 {
     private $data;
     private $file;
+    private $defaults = [
+        'config/database.yml',
+        'config/config.yml',
+    ];
 
-    public function __construct($file)
+    public function __construct($file = null)
     {
+        if (!$file) {
+            $file = $this->getDefaultConfigurationFile();
+        }
         $this->file = $file;
         $this->data = null;
     }
@@ -37,6 +44,7 @@ class ConfigManager
         }
 
         $data = Yaml::parse(file_get_contents($this->file));
+        $this->isValidConfiguration($data);
         $this->data = $data['doctrine'];
     }
 
@@ -45,5 +53,35 @@ class ConfigManager
         if (!in_array($connection, $this->getAvailableConnections())) {
             throw new \InvalidArgumentException(sprintf('Connection %s is not configured.', $connection));
         }
+    }
+
+    private function isValidConfiguration($data)
+    {
+        $this->hasDoctrineKey($data);
+        $this->hasDbalKey($data);
+    }
+
+    private function hasDoctrineKey($data)
+    {
+        if (!isset($data['doctrine'])) {
+            throw new \InvalidArgumentException('doctrine key not found at the root level of the file.');
+        }
+    }
+
+    private function hasDbalKey($data)
+    {
+        if (!isset($data['doctrine']['dbal'])) {
+            throw new \InvalidArgumentException('dbal key not found under doctrine key.');
+        }
+    }
+
+    private function getDefaultConfigurationFile()
+    {
+        foreach ($this->defaults as $file) {
+            if (file_exists($file)) {
+                return $file;
+            }
+        }
+        throw new \InvalidArgumentException('Need a configuration file a config/database.yml or config/config.yml');
     }
 }
